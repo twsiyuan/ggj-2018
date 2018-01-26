478d289121a80c9fe6a5471dbfa921f8f6dbe6c1
DESKTOP-BMTEC9Q\louis <louisalflame@gmail.com>
2018-01-27 02:53:09 +0800

    try add unit test

---
+++ Assets/Scripts/Core Game Machine/Editor/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class UnitTest {
+
+	[Test]
+	public void NewEditModeTestSimplePasses() {
+        IPassenger passenger = new IPassenger();
+	}
+    
+}
