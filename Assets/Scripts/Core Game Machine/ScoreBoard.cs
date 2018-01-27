d6a9ccb01102896b5ed91d50820766bd4e24848c
Mars <snowfish7@gmail.com>
2018-01-27 19:32:34 +0800

    New scripts.

---
+++ Assets/Scripts/Core Game Machine/ScoreBoard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{	
+
+    [SerializeField]
+    private int score;
+	
+    [SerializeField]
+    private int rage;
+
+    [SerializeField]
+    private ScoreBoardView view = new ScoreBoardView();
+
+    public void UpdateScore()
+    {
+        score++;
+        view.UpdateScore(score);
+    }
+
+    public void UpdateDepot(string value)
+    {
+        view.UpdateDepot(value);
+    }
+
+}
