f6370b7e3b9b8271d54354d61f8e37e45afbb8f8
Siyuan <easy0519@gmail.com>
2018-01-27 17:43:18 +0800

    Add entry & menu scene

---
+++ Assets/Scripts/Scenes/EntryScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EntryScene : MonoBehaviour {
+
+	[SerializeField]
+	string nextScene;
+
+	public void NextScene(){
+		SceneManager.LoadScene (this.nextScene, LoadSceneMode.Single);
+	}
+}
