4cac54f46dd205f25432802059c177f166f1b307
Mars <snowfish7@gmail.com>
2018-01-27 23:25:48 +0800

    Prevent null excelption.

--- Assets/Scripts/Core Game Machine/GameController.Utils.cs
+++ Assets/Scripts/Core Game Machine/GameController.Utils.cs
@@ -21,25 +21,18 @@ public partial class GameController
 
 	public void AddPassengerCompletion()
 	{
-		scoreBoard.AddPassengerCompletion();
+		// scoreBoard.AddPassengerCompletion();
 	}
 	
 
 	public void AddRage(int value)
 	{
-		scoreBoard.AddRage(value);
+		// scoreBoard.AddRage(value);
 	}
 
 	public void AdjustRageMax(int value)
 	{
 		scoreBoard.AdjustRageMax(value);
 	}
-
-
-	private void functionName()
-	{
-	
-	}
-	
 
 }
