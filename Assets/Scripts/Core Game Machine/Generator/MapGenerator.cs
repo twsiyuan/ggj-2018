e633b6e4e1aa95ff5598d08245501858fb0fe758
Siyuan <easy0519@gmail.com>
2018-01-27 12:06:58 +0800

    Add MapStation & MapLink (for MapGenerator)

---
+++ Assets/Scripts/Core Game Machine/Generator/MapGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour 
+{
+	[SerializeField]
+	Transform rootTransform = null;
+	
+	void Awake()
+	{
+		if (this.rootTransform != null) {
+			var stations = this.transform.GetComponentsInChildren<MapStation> ();
+			var links = this.transform.GetComponentsInChildren<MapLink> ();
+
+			foreach (var link in links) {
+				if (!link.IsValid) {
+					Debug.LogErrorFormat (link, "Link({0}) is invalid", link.name);
+				}
+			}
+
+
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Core Game Machine/Generator/MapLink.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLink : MonoBehaviour 
+{
+	[SerializeField]
+	MapStation startStation;
+
+	[SerializeField]
+	MapStation endStation;
+
+	public MapStation StartStation
+	{
+		get{
+			return this.startStation;
+		}
+	}
+
+	public MapStation EndStation
+	{
+		get{
+			return this.endStation;
+		}
+	}
+
+	public bool IsValid
+	{
+		get{
+			return this.StartStation != null && this.EndStation != null && this.StartStation == this.EndStation;
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		if (this.startStation == null || this.endStation == null) 
+		{
+			return;
+		}
+
+		var p1 = this.startStation.transform.position;
+		var p2 = this.endStation.transform.position;
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(p1, p2);
+	
+	}
+}
