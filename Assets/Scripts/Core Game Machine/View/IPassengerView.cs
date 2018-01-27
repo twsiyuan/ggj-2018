32c63d398b2a9d044d51cfad9aa19988904193b9
DESKTOP-BMTEC9Q\louis <louisalflame@gmail.com>
2018-01-27 17:17:36 +0800

    passenger View

--- Assets/Scripts/Core Game Machine/View/IPassengerView.cs
+++ Assets/Scripts/Core Game Machine/View/IPassengerView.cs
@@ -1,6 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public interface IPassengerView
 {
     void ShowViewPositionAtStation(Transform stationTransform, int order);
+
+    IEnumerator AboardBusAnimate(Transform stationTransform);
 }
