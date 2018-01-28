0948c00f59aa0cfeb36afc02a136ff8eac52a924
Siyuan <easy0519@gmail.com>
2018-01-28 11:39:13 +0800

    Add cursor

---
+++ Assets/Scripts/Scenes/CursorSetup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSetup : MonoBehaviour {
+
+	[SerializeField]
+	Texture2D normalCursor;
+
+	[SerializeField]
+	Vector2 normalCursorHotspot;
+
+	[SerializeField]
+	Texture2D clickCursor;
+
+	[SerializeField]
+	Vector2 clickCursorHotspot;
+
+	void Awake()
+	{
+		Cursor.SetCursor (this.normalCursor, this.normalCursorHotspot, CursorMode.ForceSoftware);
+		GameObject.DontDestroyOnLoad (this.gameObject);
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButton (0)) {
+			Cursor.SetCursor (this.clickCursor, this.clickCursorHotspot, CursorMode.ForceSoftware);
+		} else {
+			Cursor.SetCursor (this.normalCursor, this.normalCursorHotspot, CursorMode.ForceSoftware);
+		}
+	}
+}
