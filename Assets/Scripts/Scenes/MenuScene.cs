f088789bc83e6d189a83a475ad7f921771c746fc
Siyuan <easy0519@gmail.com>
2018-01-28 11:59:17 +0800

    Add MenuScene

---
+++ Assets/Scripts/Scenes/MenuScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour {
+	public void SwitchMenu(string levelName){
+		SceneManager.LoadScene (levelName, LoadSceneMode.Single);
+	}
+}
