bce0b57f4dedcef9c9054554e514397f2173c60b
Siyuan <easy0519@gmail.com>
2018-01-28 12:07:41 +0800

    Update name

--- Assets/Scripts/Scenes/MenuScene.cs
+++ Assets/Scripts/Scenes/MenuScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScene : MonoBehaviour {
-	public void SwitchMenu(string levelName){
+	public void SwitchLevel(string levelName){
 		SceneManager.LoadScene (levelName, LoadSceneMode.Single);
 	}
 }
